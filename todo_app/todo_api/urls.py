b24c716a278f1eb4d4c9076d72c7300b937435e2
Dawit Belayneh <dawitbelayneh9@gmail.com>
2025-12-17 00:47:35 -0900

    add urls for crud operation

--- todo_app/todo_api/urls.py
+++ todo_app/todo_api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('todos/', views.TodoItemListCreateAPIView.as_view(), name='todoitem-list-create'),
+    path('todos/<int:pk>/', views.TodoItemRetrieveUpdateDestroyAPIView.as_view(), name='todoitem-detail'),
 ]
